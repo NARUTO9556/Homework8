a2e8a815467c7eef16aa0c7e642ee4bc00206583
NARUTO9556 <gruver5263@gmail.com>
2023-04-29 23:51:10 +0800

    3-4 задача

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -5,7 +7,7 @@ public class Main {
         //task1();//обьявление массива
         //task2();//распечатка массива
         //task3();//распечатка в обратной последовательности
-        //task4();
+        task4();//нечетное в четное
     }
 
     private static void task1() {
@@ -47,4 +49,44 @@ public class Main {
         System.out.println();
     }
 
+    private static void task3(){
+        int[] arr1 = {1, 2, 3};
+        double[] arr2 = {1.57, 7.654, 9.986};
+        int[] arr3 = new int[]{2, 4, 5, 8, 10};
+
+        for (int i = arr1.length -1; i >= 0; i-- ){
+            System.out.print(arr1[i]);
+            if (i!= 0){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = arr2.length -1; i >= 0; i-- ){
+            System.out.print(arr2[i]);
+            if (i!= 0){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = arr3.length -1; i >= 0; i-- ){
+            System.out.print(arr3[i]);
+            if (i!=0){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+
+    private static void task4(){
+        System.out.println("Задача 4");
+        int[] arr1 = {1, 2, 3};
+        for (int i = 0; i < arr1.length; i++) {
+            if(arr1[i] % 2 != 0) {
+                arr1[i] += 1;
+            }
+        }
+        System.out.println(Arrays.toString(arr1));
+    }
 }
